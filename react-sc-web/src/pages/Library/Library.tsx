38edbf1b8dedb41050c03c8e4cd4108994dfbf61
Kukrynitza <ilapolityko@gmail.com>
2026-04-30 20:39:00 +0300

    fix: correct casing of Input and Form imports

--- react-sc-web/src/pages/Library/Library.tsx
+++ react-sc-web/src/pages/Library/Library.tsx
@@ -13,7 +13,7 @@ import SearchIcon from '@assets/images/Search.svg';
 import { Card } from '@components/Card/Card';
 import { CardComponentType } from '@components/Card/types';
 import { CardInfo } from '@components/CardInfo/CardInfo';
-import { Input } from '@components/input/Input';
+import { Input } from '@components/Input/Input';
 import { FEATURES } from '@constants/features';
 import { scUtils } from '@api';
 import { langToKeynode, ScTag, useTranslate, useLanguage } from 'ostis-ui-lib';
@@ -42,12 +42,12 @@ const Library = () => {
 
   useEffect(() => {
     if (FEATURES.enableContextMenuOnLibrary) {
-      scUtils.searchKeynodes('ui_search_component', 'ui_filter').then(
-        ({ uiSearchComponent, uiFilter }) => {
+      scUtils
+        .searchKeynodes('ui_search_component', 'ui_filter')
+        .then(({ uiSearchComponent, uiFilter }) => {
           if (uiSearchComponent?.value) setSearchInputAddr(uiSearchComponent.value);
           if (uiFilter?.value) setFilterAddr(uiFilter.value);
-        }
-      );
+        });
     }
   }, []);
 
